a0d872c30843512c637e65f4ec5b665d28e2f198
Soares e Neves <soareseneves@terra.com.br>
2021-05-12 09:10:02 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .target(
             name: "ColorPicKit",
-            path: "Sources",
+            path: "ColorPicKit",
             exclude: [
                 "Info.plist",
             ],

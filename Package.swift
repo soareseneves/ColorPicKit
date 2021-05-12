78917cad32a8372158aea469a3ec475d58947680
Soares e Neves <soareseneves@terra.com.br>
2021-05-12 09:02:36 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,23 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-name: "ColorPicKit")
+    name: "ColorPicKit",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS("9.0")
+    ],
+    products: [
+        .library(name: "ColorPicKit", targets: ["ColorPicKit"])
+    ],
+    targets: [
+        .target(
+            name: "ColorPicKit",
+            path: "Sources",
+            exclude: [
+                "Info.plist",
+            ],
+            resources: [.process("Resources")]
+        )
+    ]
+)
